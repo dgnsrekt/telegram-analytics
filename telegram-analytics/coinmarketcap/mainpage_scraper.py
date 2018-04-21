56eec9569d767f4ca0875b72491b618cf161af14
run2devtest <run2devtest@gmail.com>
2018-04-20 23:58:51 -0500

    mainpage init

---
+++ telegram-analytics/coinmarketcap/mainpage_scraper.py
@@ -0,0 +1,30 @@
+from coinmarketcap import Market
+# TODO: LOGGING
+
+
+def getLinks(limit=None):
+    url = 'https://coinmarketcap.com/currencies/{}'
+
+    cap = Market()
+
+    if not limit:
+        stats = cap.stats()
+        active_crypto = stats['active_currencies'] + stats['active_assets']
+        limit = active_crypto
+
+    data = cap.ticker(limit=limit)
+
+    pages = {}
+    for coin in data:
+        name = coin['id']
+        link = url.format(coin['id'])
+
+        # print()
+        # print('=' * 10)
+        # print(name, link)
+
+        pages[name] = link
+
+    return pages
+
+print(len(getLinks()))
