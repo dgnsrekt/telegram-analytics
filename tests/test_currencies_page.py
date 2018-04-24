1ed6ed7bc6e6ee1b8554c7d486c7993ba436ebac
run2devtest <run2devtest@gmail.com>
2018-04-24 09:58:28 -0500

    added more isvalidTelegramlink tests

--- tests/test_currencies_page.py
+++ tests/test_currencies_page.py
@@ -3,3 +3,15 @@ from telegram_analytics.scrapers.currencies_page import *
 
 def test_isValidTelegramLink():
     assert isValidTelegramLink('https://t.me/') == True
+
+
+def test_isValidTelegramLinkTwo():
+    assert isValidTelegramLink('https://telegram.me/') == True
+
+
+def test_isValidTelegramLinkThree():
+    assert isValidTelegramLink('http://www.telegram.me/') == True
+
+
+def test_isValidTelegramLinkFalse():
+    assert isValidTelegramLink('www.google.com') == False
