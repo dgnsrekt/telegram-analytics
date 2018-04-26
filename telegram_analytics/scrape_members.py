f26fba241c10d83a73824581b899815f372b4eb1
run2devtest <run2devtest@gmail.com>
2018-04-26 10:01:36 -0500

    added hourly scheduled parsing

--- telegram_analytics/scrape_members.py
+++ telegram_analytics/scrape_members.py
@@ -5,18 +5,22 @@ from models.telegram_members_model import cleanTelegramMembersTables  # DEBUG DE
 from scrapers.telegram import parseMemberCount
 from scrapers.mainpage import getAllMarkets
 from utils.timeit import timeit
-from time import sleep
+from utils.schedule_logger import with_logging
+
+from datetime import datetime
+from time import sleep, time
 
 import schedule
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 # DEBUG DELETE WHEN DONE
 cleanTelegramMembersTables()
 
 
 @timeit
+@with_logging
 def sample_telegram_member_count(tail=None):
     if tail:
         dataframe = Telegram.getAllTelegramNames().tail(tail)
@@ -38,10 +42,14 @@ def sample_telegram_member_count(tail=None):
 
         TelegramMembers.addData(**data)
         logging.info('COMPLETED: {} out of {}\n'.format(idx + 1, dataframe_len))
+    logging.info('TIME: {}'.format(datetime.now()))  # move to timeit
 
 
 def main():
-    schedule.every(1).hours.do(sample_telegram_member_count)
+    # schedule.every().minute.do(sample_telegram_member_count, tail=5)  # debugger
+    for idx in range(24):
+        time = '{:02d}:00'.format(idx)
+        schedule.every().day.at(time).do(sample_telegram_member_count)
 
     while True:
         schedule.run_pending()
